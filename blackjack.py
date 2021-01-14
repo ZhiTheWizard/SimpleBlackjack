2f17ff7fb39e3ab5d5ad0854f86ec0f3f6ac6091
ZhiTheWizard <zhikangxie107@gmail.com>
2021-01-13 22:06:11 -0600

    added computer to draw a card sometimes

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 card01 = random.randint(1, 11)
 card02 = random.randint(1, 11)
@@ -10,6 +11,7 @@ deck = card01 + card02
 computer = card03 + card04
 
 print("Cards: {0}".format(deck))
+print("Computer: {0}".format(computer))
 
 def start():
     print("Do you want to play? \n Enter 'yes' to play \n Enter 'no' to draw a card")
@@ -17,8 +19,9 @@ def start():
     if answer == "yes":
         play()
     elif answer == "no":
+       pcdrawcard()
        drawcard()
-
+       
 def play():
     global deck
     global computer
@@ -28,33 +31,51 @@ def play():
     elif deck == computer and computer < 22:
         tied()
     else:
-        lost()
+        lost("You lose")
 
 def drawcard():
     global deck
     cards = random.randint(1, 11)
     deck += cards
     if deck >= 22:
-        lost()
+        lost("Busted")
     else: 
         print("Cards: {0}".format(deck))
         start()
         
+def pcdrawcard():
+    global computer
+    global deck
+    probability = 3
+    cards = random.randint(1, 11)
+
+    if probability == 3 or 5 or 9 or 1:
+        computer += cards
+
+    if computer >= 22:
+        print("Opponent busted")
+        print("Cards: {0}".format(deck))
+        print("Computer: {0}".format(computer))
+        sys.exit()
+
 def win():
     global deck
+    global computer
     print("You win")
     print("Cards: {0}".format(deck))
     print("Computer: {0}".format(computer))
 
 def tied():
     global deck
+    global computer
     print("You tied")
     print("Cards: {0}".format(deck))
     print("Computer: {0}".format(computer))
 
-def lost():
+def lost(loseStatement):
     global deck
-    print("You lose")
+    global computer
+    print("You {0}".format(loseStatement))
     print("Cards: {0}".format(deck))
     print("Computer: {0}".format(computer))
 
